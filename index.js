c2cb330b97a025869478ade873949d7f39b6c8e8
addy-96 <adisin009@gmail.com>
2025-11-14 00:35:26 +0530

    .gitignore setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello");
+import express from 'express';
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('first middleware ran');
+    next(); 
+});
+
+app.get("/", (req, res) => {
+    res.send(JSON.{});
+});
+
+app.listen(8000, () => {
+    console.log('\nServer running at http://localhost:8000\n');
+});
