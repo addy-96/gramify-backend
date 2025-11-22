b111766b6a5f9b69ceb7b7183ff74c30d72f646d
aditya-investnama <aditya@investnama.com>
2025-11-22 14:36:42 +0530

    rewrite

--- routes/user_routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getUserProfile, editUserProfile } from "../controllers/user_controller.js";
-
-const router = express.Router();
-
-router.get("/",getUserProfile);
-router.put("edit",editUserProfile);
-
-export default router;
