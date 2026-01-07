adce4bbf61f7f23e9bdf867db1dac5cc61f9d0bb
addy-96 <adisin009@gmail.com>
2026-01-08 03:33:14 +0530

    add: added post schema

---
+++ models/comment.js
@@ -0,0 +1,5 @@
+import {mongoose, Schema} from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    
+});
---
+++ models/post.js
@@ -0,0 +1,14 @@
+import {mongoose,Schema} from "mongoose";
+
+const postSchema = new mongoose.Schema({
+    postCaption: {type: String},
+    postImage: {type: [String]},
+    likedBy: {type: [Schema.Types.ObjectId]},
+    comments: {type: [Schema.Types.ObjectId]},
+    shareCount: {type: Number,default: 0},
+    tags: {type: [Schema.Types.ObjectId]},
+    createdAt:{type: Date,default:Date.now}
+
+});
+
+export default mongoose.model('Post',postSchema);
